b06e904724ae797d2187ff1729772f76aacf1afa
sensecoder <sensecoding@gmail.com>
2022-07-10 01:29:08 +0500

    models first commit

---
+++ lolPlay/Data/Models/CatalogBranch.cs
@@ -0,0 +1,18 @@
+﻿namespace lolPlay.Data.Models
+{
+    public class CatalogBranch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+        public List<Media>? Medias { get; set; }
+        public List<CatalogBranch>? ChildrenBranches { get; set; }
+
+        public CatalogBranch(int id, string name, int? parentId = null)
+        {
+            Id = id;
+            Name = name;
+            ParentId = parentId;
+        }
+    }
+}
---
+++ lolPlay/Data/Models/Media.cs
@@ -0,0 +1,10 @@
+﻿namespace lolPlay.Data.Models
+{
+    public class Media
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+        public int? BranchId { get; set; }
+    }
+}
