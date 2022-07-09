99e5881efeda39743321cdf55e2a406ac0162bb9
sensecoder <sensecoding@gmail.com>
2022-07-10 01:33:34 +0500

    delete constructor

--- lolPlay/Data/Models/CatalogBranch.cs
+++ lolPlay/Data/Models/CatalogBranch.cs
@@ -8,11 +8,5 @@
         public List<Media>? Medias { get; set; }
         public List<CatalogBranch>? ChildrenBranches { get; set; }
 
-        public CatalogBranch(int id, string name, int? parentId = null)
-        {
-            Id = id;
-            Name = name;
-            ParentId = parentId;
-        }
     }
 }
